10abe67bf6af863e422d3ae7e06afef4ac07506b
Junior Pedroso <sintasebemagora@gmail.com>
2022-10-20 17:44:34 -0300

    Update

---
+++ cap6/programa0620.py
@@ -0,0 +1,18 @@
+l = [7, 4, 3, 12, 8]
+# l = [1, 2, 3, 4, 5]
+fim = 5
+print(l)
+while fim > 1:
+    trocou = False
+    x = 0
+    while x < (fim - 1):
+        if l[x] > l[x + 1]:
+            trocou = True
+            temp = l[x]
+            l[x] = l[x + 1]
+            l[x + 1] = temp
+            print(l)
+        x += 1
+    if not trocou:
+        break
+    fim -= 1
